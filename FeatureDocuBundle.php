c8362553dade678c46b545359bc022990df39ae2
Steve Olotu <s.olotu@gmx.de>
2021-01-25 16:50:16 +0100

    move file

--- FeatureDocuBundle.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SteveOlotu\FeatureDocuBundle;
-
-class FeatureDocuBundle
-{
-
-}
--- src/FeatureDocuBundle.php
+++ src/FeatureDocuBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SteveOlotu\FeatureDocuBundle;
+namespace SteveOlotu\FeatureDocu;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
